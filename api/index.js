eb48513578963294aa72ca51989a5f24b18b216b
zap <zaptorrent3@gmail.com>
2025-04-11 20:39:24 +0530

    dsa

--- api/index.js
+++ api/index.js
@@ -80,4 +80,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
 
-module.exports = Category;  // Export the Category model
+module.exports = app;
+
